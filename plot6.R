6418ada182d746200ba53152535b6e2bccb5c773
jrbattles <jason.r.battles@gmail.com>
2016-04-28 22:33:39 -0500

    Graphed Motor Vehicle (PM25) Emissions - Baltimore

---
+++ plot6.R
@@ -0,0 +1,7 @@
+# Exploratory Data Analysis - Assignment 2 - Question #6
+# Jason R. Battles
+
+# Data should already be loaded into NEI and SCC dataframes from the ObtainData.R script
+
+# Load ggplot2 library
+require(ggplot2)
